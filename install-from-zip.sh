ee515c6dddc12479a525e5dc4bf91c7ef0a5aff1
Adam Jarvis <adam@jarvis.gg>
2020-11-17 16:05:38 +0000

    Update install script

--- install-from-zip.sh
+++ install-from-zip.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
 
-XENFORO_DIR="./"
+XENFORO_DIR="."
 XENFORO_ZIP="./xenforo.zip"
 
 function install() {
-    if [[ ! -f $XENFORO_ZIP ]]; then
-        echo "xenforo.zip not present, please dowload from the account panel"
-        exit;
-    fi
-
-    echo "protecting xenforo zip by moving it temporarily into ./.github"
-    mv $XENFORO_ZIP ./.github/
+    echo "protecting xenforo zip by moving it temporarily into /tmp"
+    mv $XENFORO_ZIP /tmp/xenforo.zip
 
     if [ -f "admin.php" ]; then
         echo "Removing existing xenforo dir"
         git clean -x -f ;
     fi
 
-    mv ./.github/xenforo.zip $XENFORO_ZIP
+    mv /tmp/xenforo.zip $XENFORO_ZIP
 
 
     unzip $XENFORO_ZIP -d $XENFORO_DIR
-    mv $XENFORO_DIR/upload/** $XENFORO_DIR/
-    rm -rf $XENFORO_DIR/upload
+    cp -r $XENFORO_DIR/upload/** $XENFORO_DIR/
+    rm -Rf $XENFORO_DIR/upload XF2-INSTALL-UPGRADE.txt
     echo "Xenforo unzipped into $XENFORO_DIR"
 
+
     echo "Setting correct file permissions"
-    chmod 0755 $XENFORO_DIR;
-    chmod 0777 $XENFORO_DIR/data $XENFORO_DIR/internal_data
+
+    if [ "$(uname)" == "Darwin" ]; then
+        sudo chown -R $(whoami):staff ./
+    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+        sudo chown -R $(whoami):$(whoami) ./
+    fi
+
+    sudo chmod 0755 $XENFORO_DIR;
+    sudo chmod 0777 $XENFORO_DIR/data $XENFORO_DIR/internal_data
 }
+
+if [[ ! -f $XENFORO_ZIP ]]; then
+    echo "xenforo.zip not present, please dowload from the account panel"
+    exit;
+fi
 
 if [ -f "admin.php" ]; then
     echo "Xenforo 2 seems to already unpacked"
